a11bc9a4466f86e046419e5b85761b7610bc5882
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-09-06 02:07:21 -0500

    Set config.active_record.whitelist_attributes to true by default

--- lib/protected_attributes/railtie.rb
+++ lib/protected_attributes/railtie.rb
@@ -2,6 +2,7 @@ module ProtectedAttributes
   class Railtie < ::Rails::Railtie
     config.before_configuration do
       config.action_controller.permit_all_parameters = true
+      config.active_record.whitelist_attributes = true
     end
   end
 end
