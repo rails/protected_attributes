f608f2a1fb78c521698273e6d7057493f73d1e8d
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-10-30 00:07:51 -0500

    set whitelist_attributes option before AR sets configs

--- lib/protected_attributes/railtie.rb
+++ lib/protected_attributes/railtie.rb
@@ -1,11 +1,9 @@
 module ProtectedAttributes
   class Railtie < ::Rails::Railtie
-    config.before_configuration do
+    config.before_configuration do |app|
       config.action_controller.permit_all_parameters = true
       config.active_record.whitelist_attributes = true
-    end
 
-    initializer "active_record.set_configs" do |app|
       ActiveSupport.on_load(:active_record) do
         if app.config.active_record.delete(:whitelist_attributes)
           attr_accessible(nil)
