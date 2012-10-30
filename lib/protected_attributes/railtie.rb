11303530e57bc3a7e7a079a929be9c4ab9b5975a
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-10-29 23:47:55 -0500

    Add back config options

--- lib/protected_attributes/railtie.rb
+++ lib/protected_attributes/railtie.rb
@@ -4,5 +4,13 @@ module ProtectedAttributes
       config.action_controller.permit_all_parameters = true
       config.active_record.whitelist_attributes = true
     end
+
+    initializer "active_record.set_configs" do |app|
+      ActiveSupport.on_load(:active_record) do
+        if app.config.active_record.delete(:whitelist_attributes)
+          attr_accessible(nil)
+        end
+      end
+    end
   end
 end
