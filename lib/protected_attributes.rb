28b4796e7e1c9b015f759856770273e21b6f9ac5
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-12-19 21:34:17 -0200

    Register the load hooks as soon as the gem is required
    Obey the load order from the Gemfile to properly register the load hooks
    for AR and AC.
    
    It is too late to register the hooks from inside an initializer block
    and require the extensions, since the initializer blocks are executed
    later in the initialization process. For instance, acts-as-taggable-on
    was requiring some models when being required, force loading AR. When
    AR::Base was loaded, the initializer blocks weren't run yet, so there
    was no protected attributes extension set up, and everything exploded.
    
    By moving the load hooks registration to the moment the lib is required,
    as soon as AR::Base is loaded and the load hooks are called, protected
    attributes extensions will be properly executed, as long as the gem
    comes first in the Gemfile.

--- lib/protected_attributes.rb
+++ lib/protected_attributes.rb
@@ -2,5 +2,13 @@ require "active_model/mass_assignment_security"
 require "protected_attributes/railtie" if defined? Rails
 require "protected_attributes/version"
 
+ActiveSupport.on_load :active_record do
+  require "active_record/mass_assignment_security"
+end
+
+ActiveSupport.on_load :action_controller do
+  require "action_controller/accessible_params_wrapper"
+end
+
 module ProtectedAttributes
 end
--- lib/protected_attributes/railtie.rb
+++ lib/protected_attributes/railtie.rb
@@ -9,17 +9,9 @@ module ProtectedAttributes
 
     initializer "protected_attributes.active_record", :before => "active_record.set_configs" do |app|
       ActiveSupport.on_load :active_record do
-        require "active_record/mass_assignment_security"
-
         if app.config.respond_to?(:active_record) && app.config.active_record.delete(:whitelist_attributes)
           attr_accessible(nil)
         end
-      end
-    end
-
-    initializer "protected_attributes.action_controller" do
-      ActiveSupport.on_load :action_controller do
-        require "action_controller/accessible_params_wrapper"
       end
     end
   end
