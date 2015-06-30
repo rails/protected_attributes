10de49ae80475d38d320615ce9aeab7d40d28e70
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-06-30 04:32:51 -0300

    Change default behavior when including the gem
    Before we were permitting all the parameters and forcing to whitelist all
    the attributes. This make harder to upgrade to strong parameters because
    you can't slowly change the models to not use the attribute macros.
    
    Since now we are fallbacking to strong parameters if the macro is not
    defined on the model we should not permit all the parameters neither
    call the macro on ActiveRecord::Base.
    
    Models with macros will use the mass assignment sanitization and models
    without the macros will raise if parameters are not permitted.

--- lib/protected_attributes/railtie.rb
+++ lib/protected_attributes/railtie.rb
@@ -1,16 +1,4 @@
 module ProtectedAttributes
   class Railtie < ::Rails::Railtie
-    config.before_configuration do |app|
-      config.action_controller.permit_all_parameters = true
-      config.active_record.whitelist_attributes = true if config.respond_to?(:active_record)
-    end
-
-    initializer "protected_attributes.active_record", :before => "active_record.set_configs" do |app|
-      ActiveSupport.on_load :active_record do
-        if app.config.respond_to?(:active_record) && app.config.active_record.delete(:whitelist_attributes)
-          attr_accessible(nil)
-        end
-      end
-    end
   end
 end
