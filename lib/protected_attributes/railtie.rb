a92b1c0ac5c935b922d3042c95c82efa0be676ac
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-07-14 15:28:49 -0300

    Deprecate config.active_record.whitelist_attributes

---
+++ lib/protected_attributes/railtie.rb
@@ -0,0 +1,9 @@
+module ProtectedAttributes
+  class Railtie < ::Rails::Railtie
+    initializer "protected_attributes.active_record", :before => "active_record.set_configs" do |app|
+      if app.config.respond_to?(:active_record) && app.config.active_record.delete(:whitelist_attributes)
+        ActiveSupport::Deprection.warn "config.active_record.whitelist_attributes is deprecated and have no effect. Remove its call from the configuration."
+      end
+    end
+  end
+end
